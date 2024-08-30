d79dff8e9aa57cfa4472d42994beb9866f6d2a68
Rajendra Kumar Yadav <13816347+rajendrakumaryadav@users.noreply.github.com>
2024-08-30 19:36:45 +0000

    adding API_HOST and API_PORT to be read from environment or default as it is defined

--- src/config/api.js
+++ src/config/api.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   tileLayer: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-  host: 'http://localhost',
-  port: '3000',
+  host: process.env.API_HOST || 'http://localhost', 
+  port: process.env.API_PORT || '3000', 
   maxTaskNumber: 100,
   overpassEndpoint: 'https://overpass-api.de/api/interpreter'
 };
